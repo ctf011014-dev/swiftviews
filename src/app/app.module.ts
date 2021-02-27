3c5abbeb9625fd9de4859c5cb8a7ae4fce0441e7
wangthong <wangthong88899@gmail.com>
2021-02-27 18:17:11 +0700

    new component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,24 @@
+import { ContentDetailComponent } from './home/content/content-detail/content-detail.component';
+import { ContentComponent } from './home/content/content.component';
+import { SearchComponent } from './home/search/search.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderInfoComponent } from './home/header-info/header-info.component';
+import { MenuComponent } from './home/menu/menu.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    MenuComponent,
+    HeaderInfoComponent,
+    SearchComponent,
+    ContentComponent,
+    ContentDetailComponent
   ],
   imports: [
     BrowserModule
